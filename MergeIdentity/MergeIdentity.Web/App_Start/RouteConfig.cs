752e5e826b4eedda4f344073ccaa1c605e82fc7e
John Bissonette <jbissonette@UR.Rochester.edu>
2016-07-07 16:55:54 -0400

    added identity files and folders to project, changed namespaces and rebuilt successfully

--- MergeIdentity/MergeIdentity.Web/App_Start/RouteConfig.cs
+++ MergeIdentity/MergeIdentity.Web/App_Start/RouteConfig.cs
@@ -11,6 +11,12 @@ namespace MergeIdentity
             routes.IgnoreRoute("Content/{resource}.axd/{*pathInfo}");
 
             routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
+            );
         }
     }
 }
